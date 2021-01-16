de54c1d6f649beb30315a80c20cc4be88b21877e
leoboyerbx <contact@leoboyer.fr>
2021-01-16 22:21:56 +0100

    Add vuex vue cli

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import './index.css'
 
 import config from '@/mixins/config'
+import store from './store'
 // import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.mixin(config)
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,7 @@ module.exports = {
     extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
